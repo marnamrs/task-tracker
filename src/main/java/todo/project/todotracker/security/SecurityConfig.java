81eb268814951f50c0a652f52cad5a79f421b21e
Marina M <marnacode@gmail.com>
2023-05-15 19:05:50 +0200

    setup commit

---
+++ src/main/java/todo/project/todotracker/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package todo.project.todotracker.security;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    //interface to retrieve user information
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    //instance of AuthenticationManagerBuilder
+    @Autowired
+    private AuthenticationManagerBuilder authManagerBuilder;
+
+    //instance of DelegatingPasswordEncoder
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //TODO add SecurityFilterChain
+
+    //TODO check if custom AuthenticationFilter and AuthorizationFilter are needed
+
+
+
+
+}
