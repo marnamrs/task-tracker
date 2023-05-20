001daf18dd2eacc1bcb5ea6b3f421a3a7f164288
Marina M <marnacode@gmail.com>
2023-05-20 19:28:15 +0200

    display errors when creating a new task

--- src/main/java/todo/project/todotracker/controllers/ViewController.java
+++ src/main/java/todo/project/todotracker/controllers/ViewController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 import todo.project.todotracker.models.tasks.Task;
 import todo.project.todotracker.models.tasks.TaskDTO;
 import todo.project.todotracker.models.users.User;
@@ -23,7 +24,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
-public class HomeController {
+public class ViewController {
 
     @Autowired
     TaskService taskService;
@@ -105,7 +106,9 @@ public class HomeController {
         try {
             httpResponse.sendRedirect("/");
         } catch (IOException e) {
+            System.out.println("CATCH: ERROR --> " + e.getMessage());
             model.addAttribute("error", e.getMessage());
+            model.addAttribute("allUsers", userService.getAllUsers());
             return "newtask";
         }
         return null;
